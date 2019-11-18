5bf1a7fb9fc404eb44d04453730ce2520e8e33ab
Regino P <regino5298@gmail.com>
2019-11-18 13:36:39 -0700

    init commit

---
+++ front-end/src/components/SignUpForm.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { withformik, Form, Field } from 'formik';
+import * as yup from 'yup';
+
+const NavStyle = styled.nav`
+    display: flex;
+    align-items: center;
+    background-color: #235B2D;
+`;
+const H1Style = styled.h1`
+    width: 50%;
+    display: flex;
+    padding-left: 10px;
+    color: white;
+`;
+const H4Style = styled.h4`
+    width: 50%;
+    display: flex;
+    justify-content: flex-end;
+    padding-right: 10px;
+    color: white;
+`;
+        <NavStyle>
+            <H1Style>Plant Parenthood</H1Style>
+            <H4Style>Icon</H4Style>
+        </NavStyle>
+
