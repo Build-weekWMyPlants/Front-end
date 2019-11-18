5c3486dc3541a1c1fbecae50fe1b8eb4d3ba49bf
Luis Floresgit config --global user.name Luis <Lflores02142@gmail.com>
2019-11-18 14:45:54 -0700

    finished login actions

---
+++ front-end/src/actions/Loginactions.js
@@ -0,0 +1,36 @@
+import axios from "axios"
+
+export const LOGIN_START = "LOGIN_START"
+export const LOGIN_SUCCESS = "LOGIN_SUCCESS"
+export const LOGIN_FAIL = "LOGIN_FAIL"
+
+//login actions
+export const loginStart = (data) => ({
+    type: LOGIN_START,
+    payload: data
+});
+export const loginSccess = () => ({
+    type: LOGIN_SUCCESS,
+    payload: {}
+})
+export const loginFail = error => ({
+    tpye: LOGIN_FAIL,
+    Payload: {
+        error
+    }
+})
+
+export const login = e => {
+    e.preventDefault();
+    axios
+    .get("/signin" , data)
+    .then(response => {
+        console.log("LOGIN",response.data);
+        dispatch(loginSuccess(response.data));
+        const { data } = response;
+        localStorage.setItem("token", data.payload);
+    })
+    .catch(error => {
+        dispatch(loginFail(error.response))
+    })
+}
